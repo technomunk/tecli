90402c68396891e9fbf48c308a9b5a0f6d0a8130
Griffone <thegriffones@gmail.com>
2022-07-01 18:38:30 +0200

    Add the most trivial of functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+//! Technomunk's random CLI-based stuff
+
+#![warn(clippy::all)]
+
+use clap::Parser;
+
+/// Trivial CLI command that just prints hello world
+#[derive(Parser, Debug)]
+struct Args {}
+
 fn main() {
+    // Parse empty args, as the user may have supplied a help flag
+    let _args = Args::parse();
+
     println!("Hello, world!");
 }
